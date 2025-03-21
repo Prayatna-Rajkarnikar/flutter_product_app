a5119eb055da173e05adb7d5a9f5b1ba5bab5f63
Prayatna Rajakrnikar <prayatna.rajkarnikar@gmail.com>
2025-03-21 13:43:52 +0545

    feat: added api service and model class

---
+++ lib/models/product.dart
@@ -0,0 +1,22 @@
+class Product {
+  final int id;
+  final String title;
+  final String image;
+  final double price;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.price,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> product) {
+    return Product(
+      id: product['id'],
+      title: product['title'],
+      image: product['thumbnail'],
+      price: product['price'],
+    );
+  }
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:product_app/models/product.dart';
+
+class ApiService {
+  Future<List<Product>> fetchProducts() async {
+    final response = await http.get(
+      Uri.parse("https://dummyjson.com/products"),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return (data['products'] as List)
+          .map((e) => Product.fromJson(e))
+          .toList();
+    } else {
+      throw Exception("Failed to get products");
+    }
+  }
+
+  Future<Product> fetchProductDetail(int id) async {
+    final response = await http.get(
+      Uri.parse(": https://dummyjson.com/products/{id}"),
+    );
+    if (response.statusCode == 200) {
+      return Product.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to load product details");
+    }
+  }
+}
+
+Future<List<Product>> searchProducts(String query) async {
+  final response = await http.get(
+    Uri.parse("https://dummyjson.com/products/search?q=your_query."),
+  );
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    return (data['products'] as List).map((e) => Product.fromJson(e)).toList();
+  } else {
+    throw Exception("Failed to search products");
+  }
+}
+
+Future<List<Product>> sortProducts() async {
+  final response = await http.get(
+    Uri.parse("https://dummyjson.com/products?sortBy=title&order=asc"),
+  );
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    return (data['products'] as List).map((e) => Product.fromJson(e)).toList();
+  } else {
+    throw Exception("Failed to sort products");
+  }
+}
