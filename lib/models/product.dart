87c25aa1dbb7572ea15258c12e8996e79d9f8874
Prayatna Rajakrnikar <prayatna.rajkarnikar@gmail.com>
2025-03-21 15:34:29 +0545

    refactor: model class

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,22 +1,34 @@
 class Product {
   final int id;
   final String title;
+  final String description;
+  final String category;
   final String image;
   final double price;
+  final double discountPercentage;
+  final double rating;
 
   Product({
     required this.id,
     required this.title,
+    required this.description,
+    required this.category,
     required this.image,
     required this.price,
+    required this.discountPercentage,
+    required this.rating,
   });
 
   factory Product.fromJson(Map<String, dynamic> product) {
     return Product(
       id: product['id'],
       title: product['title'],
+      description: product['description'],
+      category: product['category'],
       image: product['thumbnail'],
       price: product['price'].toDouble(),
+      rating: product['rating'].toDouble(),
+      discountPercentage: product['discountPercentage'].toDouble(),
     );
   }
 }
